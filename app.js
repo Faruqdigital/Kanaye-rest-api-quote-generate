f1f6b453227c405893078dd9027783df316ce4e7
Faruq Hossain <faruqdigital01@gmail.com>
2023-05-14 23:28:09 +0600

    user

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-function fetchApi() {
-    fetch("https://api.kanye.rest")
-        .then(res => res.json())
-        .then(data => showQuote(data.quote));
-}
+// function fetchApi() {
+//     fetch("https://api.kanye.rest")
+//         .then(res => res.json())
+//         .then(data => showQuote(data.quote));
+// }
 
-function showQuote(data) {
-    document.getElementById("quote").innerHTML = data;
-}
+// function showQuote(data) {
+//     document.getElementById("quote").innerHTML = data;
+// }
 
-document.getElementById("generate").addEventListener("click", () => {
-    fetchApi();
-})
+// document.getElementById("generate").addEventListener("click", () => {
+//     fetchApi();
+// })
+
+
+// const userListEl = document.getElementById("userList")
+// console.log(userListEl);
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//       .then((response) => response.json())
+//       .then((users) => showUser(users));
+
+// function showUser(user) {
+//     console.log("All user:", user);
+//     for (i = 0; i < user.length; i++){
+//         console.log(user[i].title);
+//         userListEl.innerHTML += `<li>${user[i].title}, ${user[i].userId}</li>`
+//     }
+// }
+
